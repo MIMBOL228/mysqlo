645abaefda31940b83673218a7b6b1d9ed4f92a2
Александр <59563952+MIMBOL228@users.noreply.github.com>
2022-05-03 23:48:10 +0500

    Вроде - как улучшаемся

--- mysqlo.php
+++ mysqlo.php
@@ -10,16 +10,16 @@ class mysqlo{
             ); // Создание подключения
             $this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) { // При ошибке
-            die('Ошибка подключения Базы Данных: ' . $exception->getMessage()); // Пишем её
+            trigger_error('Ошибка подключения Базы Данных: ' . $exception->getMessage(), E_USER_ERROR); // Пишем её
         }
     }
     function query(string $sql, array $parameters, bool $all = false){ // Функция выполнения запроса к MySql
         try {
             $statement = $this->connect->prepare($sql); // Подготавлеваем запрос
-            $newParameters = [];
-            foreach ($parameters as $key => $value) {
-                $newParameters[':' . $key] = $value; //Добавляем каждому имени параметра :
-            }
+            $newParameters = array_combine(
+                array_map(function($k){ return ':'.$k; }, array_keys($parameters))
+                , $parameters
+            );
             $statement->execute($newParameters); // Выполняем запрос
             if($all){ // Если передано, что передавать все полученные данные, то
                 return $statement->fetchAll(PDO::FETCH_ASSOC); // Передаём все даннные
@@ -27,7 +27,7 @@ class mysqlo{
                 return $statement->fetch(PDO::FETCH_ASSOC); // Передаём первые попавшиеся
             }
         } catch (Exception $exception) { // При ошибке
-            die('Ошибка запроса: ' . $exception->getMessage()); // Пишем её
+            trigger_error( 'Ошибка запроса: ' . $exception->getMessage(), E_USER_ERROR); // Пишем её
         }
     }
     function transaction(array $query_list){ // Функция удобной транзакции
@@ -35,16 +35,16 @@ class mysqlo{
             $this->connect->beginTransaction(); // Объявляем начало транзакции
             foreach($query_list as $sql => $parameters){ // Проходимся по каждому запросу
                 $statement = $this->connect->prepare($sql); // Подготавлеваем запрос
-                $newParameters = [];
-                foreach ($parameters as $key => $value) {
-                    $newParameters[':' . $key] = $value; // Добавляем каждому имени параметра :
-                }
+                $newParameters = array_combine(
+                    array_map(function($k){ return ':'.$k; }, array_keys($parameters))
+                    , $parameters
+                );
                 $statement->execute($newParameters); //Выполняем запрос
             }
             $this->connect->commit(); // Сохраняем изменения
         } catch (Exception $exception) { // Если что-то пошло не так
             $this->connect->rollBack(); // Откатываем (отменяем) изменения
-            die('Ошибка транзакции: ' . $exception->getMessage()); // И выводим ошибку
+            trigger_error('Ошибка транзакции: ' . $exception->getMessage(), E_USER_ERROR); // И выводим ошибку
         }
     }
 }
